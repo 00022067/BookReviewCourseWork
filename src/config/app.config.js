4e8806bb64cb392795c33311f3b794634a3dbf2f
00022067 <00022067wiut@gmail.com>
2025-04-03 15:01:40 +0500

    Set default port to 3000

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -2,4 +2,4 @@ import { config } from 'dotenv'
 
 config()
 
-export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 9000
+export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000
